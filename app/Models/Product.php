93eaea51d1c25a87d37448c27dbc06482f3ee657
dipak022 <dipakd3ebnath4002@gmail.com>
2022-04-22 02:36:36 +0600

    Complete auction system

---
+++ app/Models/Product.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+  
+    protected $fillable = [
+        'name',
+        'category_id',
+        'details',
+        'regular_price',
+        'starting_bidding_amount',
+        'bidding_end_date',
+        'image',
+        'new',
+        'featured',
+        'active',
+    ];
+}
---
+++ routes/superadmin.php
@@ -0,0 +1,33 @@
+<?php
+
+Route::group(['prefix'=>'superadmin', 'middleware'=>['isSuperadmin','auth','PreventBackHistory']], function(){
+    Route::get('dashboard',[App\Http\Controllers\Superadmin\SuperAdminController::class,'index'])->name('superadmin.dashboard');
+    
+
+
+    // manage banner route here 
+    Route::resource('/banner', App\Http\Controllers\Superadmin\BannerController::class);
+    Route::post('/banner_status', [App\Http\Controllers\Superadmin\BannerController::class, 'BannerStatus'])->name('banner.status');
+
+    // manage category route here 
+    Route::resource('/category', App\Http\Controllers\Superadmin\CategoryController::class);
+    Route::post('/category_status', [App\Http\Controllers\Superadmin\CategoryController::class, 'CategoryStatus'])->name('category.status');
+
+    // manage product route here 
+    Route::resource('/product', App\Http\Controllers\Superadmin\ProductController::class);
+    Route::post('/product_status', [App\Http\Controllers\Superadmin\ProductController::class, 'ProductStatus'])->name('product.status');
+    Route::post('/new_status', [App\Http\Controllers\Superadmin\ProductController::class, 'NewStatus'])->name('new.status');
+    Route::post('/featured_status', [App\Http\Controllers\Superadmin\ProductController::class, 'FeaturedStatus'])->name('featured.status');
+
+
+     // manage question route here 
+     Route::resource('/setting', App\Http\Controllers\Superadmin\SetingController::class);
+
+     // manage question route here 
+     Route::resource('/user_account', App\Http\Controllers\Superadmin\UserAccountController::class);
+     Route::post('/user_account_status', [App\Http\Controllers\Superadmin\UserAccountController::class, 'UserAcountStatus'])->name('user_account.status');
+
+     Route::resource('/problem', App\Http\Controllers\Superadmin\ProblemController::class);
+
+
+});
